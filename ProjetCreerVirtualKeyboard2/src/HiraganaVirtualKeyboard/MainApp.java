3321db78cd299e31de8b1a79008200c82cf62e8e
Stéphanie Durante <110696367+SutefaniD@users.noreply.github.com>
2024-04-23 18:21:12 +0200

    Enlever debug, choix de police environnement local Police choisie : Meiryo

--- ProjetCreerVirtualKeyboard2/src/HiraganaVirtualKeyboard/MainApp.java
+++ ProjetCreerVirtualKeyboard2/src/HiraganaVirtualKeyboard/MainApp.java
@@ -23,7 +23,6 @@ public class MainApp extends JFrame {
 	private JButton convertButton;
 	private JPanel topPanel;
 	private JTextArea outputTextArea;
-	
 
 	public MainApp() {
 
@@ -32,33 +31,22 @@ public class MainApp extends JFrame {
 		inputTextArea = new JTextArea();
 		JScrollPane topScrollPane = new JScrollPane(inputTextArea);
 		topScrollPane.setPreferredSize(new Dimension(200, 50));
-		
+
 		convertButton = new JButton("Convertir");
-		
+
 		topPanel = new JPanel(new BorderLayout());
 		topPanel.add(topScrollPane, BorderLayout.CENTER);
 		topPanel.add(convertButton, BorderLayout.SOUTH);
-		
+
 		outputTextArea = new JTextArea();
 		outputTextArea.setEditable(false);
-		//outputTextArea.setFont(new Font("Serif", Font.CENTER_BASELINE, 20));
 		outputTextArea.setFont(new Font("Meiryo", Font.PLAIN, 18));
-		
-		JScrollPane bottomScrollPane = new JScrollPane(outputTextArea);
-				
-		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, topPanel, bottomScrollPane);
-        getContentPane().add(splitPane);
-		
-        
-//        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-//        Font[] allFonts = ge.getAllFonts();
-//        for (Font font : allFonts) {
-//            System.out.println("all fonts: " +font.getFontName(Locale.FRANCE));
-//        }
-        System.out.println("Font utilisée: " + outputTextArea.getFont());
 
-		
-        
+		JScrollPane bottomScrollPane = new JScrollPane(outputTextArea);
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, topPanel, bottomScrollPane);
+		getContentPane().add(splitPane);
+
 		// Bouton pour convertir en Hiragana
 		convertButton.addActionListener(new ActionListener() {
 			@Override
@@ -66,50 +54,30 @@ public class MainApp extends JFrame {
 				convertToHiragana();
 			}
 		});
-		
+
 		// Conversion en tapant "Entrée"
 		inputTextArea.addKeyListener(new KeyListener() {
-			
+
 			@Override
-			public void keyTyped(KeyEvent e) {				
+			public void keyTyped(KeyEvent e) {
 			}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 			}
-			
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 					convertToHiragana();
 					e.consume(); // empêcher l'ajout d'un saut de ligne dans inputTextArea
 				}
-				
+
 			}
 		});
 
-
-		
-		// Ajouter un DocumentListener pour détecter les changements dans la JTextArea
-//        inputTextArea.getDocument().addDocumentListener(new DocumentListener() {
-//            @Override
-//            public void insertUpdate(DocumentEvent e) {
-//                convertToHiragana();
-//            }
-//
-//            @Override
-//            public void removeUpdate(DocumentEvent e) {
-//                //convertToHiragana();
-//            }
-//
-//            @Override
-//            public void changedUpdate(DocumentEvent e) {
-//                //convertToHiragana();
-//            }
-//        });
-
 	}
-	
+
 	private void convertToHiragana() {
 		String inputText = inputTextArea.getText().trim().toLowerCase();
 		System.out.println("Input Text: " + inputText); // Debug
@@ -117,14 +85,15 @@ public class MainApp extends JFrame {
 		if (!inputText.isEmpty()) {
 			RomajiToHiragana converter = new RomajiToHiragana();
 			StringBuilder result = converter.convert(inputText);
-			outputTextArea.append(result.toString() + "\n"); // Ajouter le contenu sur une nouvelle ligne dans outputTextArea
+			outputTextArea.append(result.toString() + "\n"); // Ajouter le contenu sur une nouvelle ligne dans
+																// outputTextArea
 			inputTextArea.setText(""); // Effacer le contenu de inputTextArea
 		}
-		
+
 	}
 
 	private static void createAndShowGUI() {
-		
+
 		MainApp app = new MainApp();
 		app.setTitle("Japanese Text Editor");
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
